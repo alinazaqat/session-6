01ee1278da240b50602a168628c12c85f5b844e7
alinazaqat <alinazaqatali8@gmail.com>
2024-06-03 18:51:51 +1000

    Create session6.py
    first change comitted

---
+++ session6.py
@@ -0,0 +1,4 @@
+# Topic: The for Loop ( Count- Controlled)
+Continents = ["asia", "africa", "north america", "south america", "antarctica", "australia"]
+for continent in continents:
+print(f"I like {australia}!")
